ce4ea9744ff43749026f099702ce179987539f6c
Yutian Li <kevinleeone@gmail.com>
2014-04-18 16:14:44 +0800

    [master] Mapping error?

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <cstdio>
+#include <iostream>
 
 using namespace std;
 
@@ -15,10 +16,12 @@ int main() {
     int m, n, k;
     float alpha, beta;
     clock_t startTime;
+    m = 16;
+    k = 24 * 9 * 9;
+    n = MAX_LENGTH;
+    alpha = beta = 1.0;
 
     assert(cublasCreate(&cublasHandle) == CUBLAS_STATUS_SUCCESS);
-    m = n = k = MAX_LENGTH;
-    alpha = beta = 1.0;
     assert(cudaMalloc(&devPtr[0], m * k * sizeof(float)) == cudaSuccess);
     assert(cudaMalloc(&devPtr[1], k * n * sizeof(float)) == cudaSuccess);
     assert(cudaMalloc(&devPtr[2], m * n * sizeof(float)) == cudaSuccess);
@@ -28,10 +31,20 @@ int main() {
 
     for (int size = 8; size <= MAX_LENGTH; size <<= 1) {
         int repeat = (MAX_LENGTH / size);
-        m = n = k = MAX_LENGTH;
+        n = size;
         startTime = clock();
         while (repeat--) {
-            assert(cublasSetMatrix(m, k, sizeof(float), hostPtr[0], k, devPtr[0], k) == CUBLAS_STATUS_SUCCESS);
+            // assert(cublasSetMatrix(m, k, sizeof(float), hostPtr[0], k, devPtr[0], k) == CUBLAS_STATUS_SUCCESS);
+            cublasStatus_t a = cublasSetMatrix(m, k, sizeof(float), hostPtr[0], k, devPtr[0], k);
+            if (a == CUBLAS_STATUS_NOT_INITIALIZED) {
+                cout << "not initialized" << endl;
+            } else if (a == CUBLAS_STATUS_INVALID_VALUE) {
+                cout << " invalid value" << endl;
+            } else if (a == CUBLAS_STATUS_MAPPING_ERROR) {
+                cout << "mapping error" << endl;
+            } else {
+                exit(5);
+            }
             assert(cublasSetMatrix(k, n, sizeof(float), hostPtr[1], n, devPtr[1], n) == CUBLAS_STATUS_SUCCESS);
             assert(cublasSetMatrix(m, n, sizeof(float), hostPtr[2], n, devPtr[0], n) == CUBLAS_STATUS_SUCCESS);
             assert(cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, (const float*) devPtr[1], n, (const float*) devPtr[0], k, &beta, (float*) devPtr[2], n) == CUBLAS_STATUS_SUCCESS);
