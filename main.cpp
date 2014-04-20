a2825dc777d9ac4630b057441e8ce417c13cb42c
Yutian Li <kevinleeone@gmail.com>
2014-04-20 19:46:32 +0800

    [master] First testing version

--- main.cpp
+++ main.cpp
@@ -8,41 +8,55 @@
 
 using namespace std;
 
-#define MAX_LENGTH (1 << 16)
+#define M_MAX 256
+#define K_MAX 32768
+#define N_MAX 16344
+
+/*
+ *         K                  N
+ *    ____________       ____________
+ *   |            |     |            |
+ *   |            |     |            |
+ *   |   M_{1}    |     |    M_{2}   |
+ * M |            | * K |            |
+ *   |            |     |            |
+ *   |____________|     |____________|
+ *
+ */
 
 int main() {
     cublasHandle_t cublasHandle;
     void* devPtr[3], * hostPtr[3];
-    int m, n, k;
     float alpha, beta;
     clock_t startTime;
-    m = 128;
-    k = 24 * 9 * 9;
-    n = MAX_LENGTH;
     alpha = beta = 1.0;
 
     assert(cublasCreate(&cublasHandle) == CUBLAS_STATUS_SUCCESS);
-    assert(cudaMalloc(&devPtr[0], m * k * sizeof(float)) == cudaSuccess);
-    assert(cudaMalloc(&devPtr[1], k * n * sizeof(float)) == cudaSuccess);
-    assert(cudaMalloc(&devPtr[2], m * n * sizeof(float)) == cudaSuccess);
-    assert(hostPtr[0] = malloc(m * k * sizeof(float)));
-    assert(hostPtr[1] = malloc(k * n * sizeof(float)));
-    assert(hostPtr[2] = malloc(m * n * sizeof(float)));
+    assert(cudaMalloc(&devPtr[0], M_MAX * K_MAX * sizeof(float)) == cudaSuccess);
+    assert(cudaMalloc(&devPtr[1], K_MAX * N_MAX * sizeof(float)) == cudaSuccess);
+    assert(cudaMalloc(&devPtr[2], M_MAX * N_MAX * sizeof(float)) == cudaSuccess);
+    assert(hostPtr[0] = malloc(M_MAX * K_MAX * sizeof(float)));
+    assert(hostPtr[1] = malloc(K_MAX * N_MAX * sizeof(float)));
+    assert(hostPtr[2] = malloc(M_MAX * N_MAX * sizeof(float)));
 
-    for (int size = 8; size <= MAX_LENGTH; size <<= 1) {
-        int repeat = (MAX_LENGTH / size);
-        n = size;
-        startTime = clock();
-        while (repeat--) {
-            assert(cublasSetMatrix(k, m, sizeof(float), hostPtr[0], k, devPtr[0], k) == CUBLAS_STATUS_SUCCESS);
-            assert(cublasSetMatrix(n, k, sizeof(float), hostPtr[1], n, devPtr[1], n) == CUBLAS_STATUS_SUCCESS);
-            assert(cublasSetMatrix(n, m, sizeof(float), hostPtr[2], n, devPtr[2], n) == CUBLAS_STATUS_SUCCESS);
-            assert(cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, (const float*) devPtr[1], n, (const float*) devPtr[0], k, &beta, (float*) devPtr[2], n) == CUBLAS_STATUS_SUCCESS);
-            assert(cublasGetMatrix(k, m, sizeof(float), devPtr[0], k, hostPtr[0], k) == CUBLAS_STATUS_SUCCESS);
-            assert(cublasGetMatrix(n, k, sizeof(float), devPtr[1], n, hostPtr[1], n) == CUBLAS_STATUS_SUCCESS);
-            assert(cublasGetMatrix(n, m, sizeof(float), devPtr[2], n, hostPtr[2], n) == CUBLAS_STATUS_SUCCESS);
+    for (int m = 64; m <= M_MAX; m <<= 1) {
+        for (int k = 64; k <= K_MAX; k <<= 1) {
+            printf("M: %d, K: %d\n", m, k);
+            for (int n = 128; n <= N_MAX; n <<= 1) {
+                int repeat = (M_MAX / m) * (N_MAX / n) * (K_MAX / k);
+                startTime = clock();
+                while (repeat--) {
+                    assert(cublasSetMatrix(k, m, sizeof(float), hostPtr[0], k, devPtr[0], k) == CUBLAS_STATUS_SUCCESS);
+                    assert(cublasSetMatrix(n, k, sizeof(float), hostPtr[1], n, devPtr[1], n) == CUBLAS_STATUS_SUCCESS);
+                    assert(cublasSetMatrix(n, m, sizeof(float), hostPtr[2], n, devPtr[2], n) == CUBLAS_STATUS_SUCCESS);
+                    assert(cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, (const float*) devPtr[1], n, (const float*) devPtr[0], k, &beta, (float*) devPtr[2], n) == CUBLAS_STATUS_SUCCESS);
+                    assert(cublasGetMatrix(k, m, sizeof(float), devPtr[0], k, hostPtr[0], k) == CUBLAS_STATUS_SUCCESS);
+                    assert(cublasGetMatrix(n, k, sizeof(float), devPtr[1], n, hostPtr[1], n) == CUBLAS_STATUS_SUCCESS);
+                    assert(cublasGetMatrix(n, m, sizeof(float), devPtr[2], n, hostPtr[2], n) == CUBLAS_STATUS_SUCCESS);
+                }
+                printf("N: %d, time: %lf\n", n, (double) (clock() - startTime) / CLOCKS_PER_SEC);
+            }
         }
-        printf("Size: %d, time: %lf\n", size, (double) (clock() - startTime) / CLOCKS_PER_SEC);
     }
 
     free(hostPtr[2]);
