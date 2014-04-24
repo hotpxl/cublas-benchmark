a395c6c55e6d4b1a227f4e1ffb39da33f7dbbb23
Yutian Li <kevinleeone@gmail.com>
2014-04-24 14:43:14 +0800

    [master] Synchronize after calculation

--- main.cpp
+++ main.cpp
@@ -33,6 +33,22 @@ int main() {
     assert(cublasCreate(&cublasHandle) == CUBLAS_STATUS_SUCCESS);
     assert(cudaMalloc(&devPtr, (1 << 30)) == cudaSuccess);
     assert(hostPtr = malloc(1 << 30));
+
+#if 0
+    int m = mCandidate[2];
+    int k = kCandidate[3];
+    int n = 1024;
+    gettimeofday(&tv1, 0);
+
+    cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, (const float*) devPtr, n, (const float*) devPtr, k, &beta, (float*) devPtr, n);
+    cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, (const float*) devPtr, n, (const float*) devPtr, k, &beta, (float*) devPtr, n);
+    cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, (const float*) devPtr, n, (const float*) devPtr, k, &beta, (float*) devPtr, n);
+    cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, (const float*) devPtr, n, (const float*) devPtr, k, &beta, (float*) devPtr, n);
+    assert(cublasGetMatrix(1, 1, sizeof(float), devPtr, 1, hostPtr, 1) == CUBLAS_STATUS_SUCCESS);
+
+    gettimeofday(&tv2, 0);
+    printf("For n = %d, time: %lf\n", n, (double) (tv2.tv_sec - tv1.tv_sec) * 1000000 + tv2.tv_usec - tv1.tv_usec);
+#else
     for (int mIdx = 0; mIdx < 3; ++mIdx) {
         for (int kIdx = 0; kIdx < 4; ++kIdx) {
             int m = mCandidate[mIdx];
@@ -50,11 +66,13 @@ int main() {
                 while (repeat--) {
                     assert(cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, (const float*) devPtr, n, (const float*) devPtr, k, &beta, (float*) devPtr, n) == CUBLAS_STATUS_SUCCESS);
                 }
+                assert(cublasGetMatrix(128, 128, sizeof(float), devPtr, 128, hostPtr, 128) == CUBLAS_STATUS_SUCCESS);
                 gettimeofday(&tv2, 0);
                 printf("For n = %d, time: %lf\n", n, (double) (tv2.tv_sec - tv1.tv_sec) * 1000000 + tv2.tv_usec - tv1.tv_usec);
             }
         }
     }
+#endif
     free(hostPtr);
     assert(cudaFree(devPtr) == cudaSuccess);
     cublasDestroy(cublasHandle);
