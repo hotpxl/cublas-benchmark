54b3581250d7168b2954eddc25eb39fe62665805
Yutian Li <kevinleeone@gmail.com>
2014-05-12 15:37:41 +0800

    [overlap-test] Shit, no speedup observed

--- main.cpp
+++ main.cpp
@@ -23,49 +23,49 @@ using namespace std;
 
 int main() {
     cublasHandle_t cublasHandle;
-    void* devPtr, * hostPtr;
-    float alpha, beta;
-    int mCandidate[] = {96, 256, 384};
-    int kCandidate[] = {363, 2304, 2400, 3456};
+    void* devPtr[6];
+    float alpha = 1.0, beta = 1.0;
     clock_t startTime;
     struct timeval tv1, tv2;
-    alpha = beta = 1.0;
     assert(cublasCreate(&cublasHandle) == CUBLAS_STATUS_SUCCESS);
-    assert(cudaMalloc(&devPtr, (1 << 30)) == cudaSuccess);
-    assert(hostPtr = malloc(1 << 30));
-    // cudaStream_t stream[16];
-    // for (int i = 0; i < 16; ++i) {
-    //     assert(cudaStreamCreate(stream + i) == cudaSuccess);
-    // }
-    for (int mIdx = 0; mIdx < 3; ++mIdx) {
-        for (int kIdx = 0; kIdx < 4; ++kIdx) {
-            int m = mCandidate[mIdx];
-            int k = kCandidate[kIdx];
-            // printf("Using m = %d, k = %d\n", m, k);
-            int nMax = (1 << 28) / k;
-            int nMaxRounded;
-            asm("bsrl %1, %0"
-                    : "=r" (nMaxRounded)
-                    : "r" (nMax));
-            for (int n = 512; n <= (1 << nMaxRounded); n <<= 1) {
-                int repeat = ((double) ((unsigned long long) 1 << 39)) / m / k / n;
-                int i = repeat;
-                gettimeofday(&tv1, 0);
-                while (i--) {
-                    // assert(cublasSetStream(cublasHandle, stream[i % 16]) == CUBLAS_STATUS_SUCCESS);
-                    assert(cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, (const float*) devPtr, n, (const float*) devPtr, k, &beta, (float*) devPtr, n) == CUBLAS_STATUS_SUCCESS);
-                }
-                assert(cudaDeviceSynchronize() == cudaSuccess);
-                gettimeofday(&tv2, 0);
-                printf("%d %d %d %d %lf\n", m, k, n, repeat, (double) (tv2.tv_sec - tv1.tv_sec) * 1000000 + tv2.tv_usec - tv1.tv_usec);
-            }
-        }
+    assert(cudaMalloc(&devPtr[0], 96 * 363 * 4) == cudaSuccess);
+    assert(cudaMalloc(&devPtr[1], 363 * 401408 * 4) == cudaSuccess);
+    assert(cudaMalloc(&devPtr[2], 96 * 401408 * 4) == cudaSuccess);
+    assert(cudaMalloc(&devPtr[3], 384 * 3456 * 4) == cudaSuccess);
+    assert(cudaMalloc(&devPtr[4], 3456 * 21632 * 4) == cudaSuccess);
+    assert(cudaMalloc(&devPtr[5], 384 * 21632 * 4) == cudaSuccess);
+    cudaStream_t stream[16];
+    for (int i = 0; i < 16; ++i) {
+        assert(cudaStreamCreate(stream + i) == cudaSuccess);
     }
-    // for (int i = 0; i < 16; ++i) {
-    //     assert(cudaStreamDestroy(stream[i]) == cudaSuccess);
+    int repeat = 30;
+    int m1 = 96;
+    int k1 = 363;
+    int n1 = 401408;
+    int i1 = repeat;
+    int m2 = 384;
+    int k2 = 3456;
+    int n2 = 21632;
+    int i2 = repeat;
+    gettimeofday(&tv1, 0);
+    while (i1--) {
+        assert(cublasSetStream(cublasHandle, stream[i1 % 16]) == CUBLAS_STATUS_SUCCESS);
+        assert(cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n1, m1, k1, &alpha, (const float*) devPtr[1], n1, (const float*) devPtr[0], k1, &beta, (float*) devPtr[2], n1) == CUBLAS_STATUS_SUCCESS);
+        assert(cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n2, m2, k2, &alpha, (const float*) devPtr[4], n2, (const float*) devPtr[3], k2, &beta, (float*) devPtr[5], n2) == CUBLAS_STATUS_SUCCESS);
+    }
+    //
+    // while (i2--) {
+    //     assert(cublasSetStream(cublasHandle, stream[i2 % 16]) == CUBLAS_STATUS_SUCCESS);
     // }
-    free(hostPtr);
-    assert(cudaFree(devPtr) == cudaSuccess);
+    assert(cudaDeviceSynchronize() == cudaSuccess);
+    gettimeofday(&tv2, 0);
+    printf("%lf\n", (double) (tv2.tv_sec - tv1.tv_sec) * 1000000 + tv2.tv_usec - tv1.tv_usec);
+    for (int i = 0; i < 16; ++i) {
+        assert(cudaStreamDestroy(stream[i]) == cudaSuccess);
+    }
+    for (int i = 0; i < 6; ++i) {
+        assert(cudaFree(devPtr[i]) == cudaSuccess);
+    }
     cublasDestroy(cublasHandle);
     return 0;
 }
